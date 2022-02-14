d1f609f89857c3824f899bb68fe36cd1590fe2db
Aysel <aysel2920@gmail.com>
2022-02-14 18:56:19 +0100

    initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Fun Facts about React</h1>
+        <ul className = "main--facts">
+            <li>First Reason</li>
+            <li>Second Reason</li>
+            <li>Third Reason</li>
+        </ul>
+        </main>
+    )
+}
