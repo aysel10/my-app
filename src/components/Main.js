9f7bda41e459f8a35fa243a6ed31ba64ea49ab75
Aysel <aysel2920@gmail.com>
2022-02-14 23:11:48 +0100

    styles css

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Main() {
     return (
         <main>
-            <h1>Fun Facts about React</h1>
+            <h1 className="main--title">Fun Facts about React</h1>
         <ul className = "main--facts">
             <li>First Reason</li>
             <li>Second Reason</li>
